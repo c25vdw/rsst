536f5371757cb16ccd9fcd68246400f265c2bd6d
c25vdw <lucaszeng99@gmail.com>
2021-01-29 16:37:16 -0700

    chore: clean up old App codes

--- src/states/feeding.rs
+++ src/states/feeding.rs
@@ -1,48 +1,8 @@
 use crate::{Buffer, Channel, CursorDir, Result, State, Subscription};
 
-use anyhow::anyhow;
-use std::fs::File;
-use std::io::{self, Write};
-use std::path::Path;
-
 use super::landing::Landing;
 use crate::buffer::consts::*;
-
-pub struct FeedController {
-    pub buf: Buffer,
-}
-
-// impl FeedController {
-//     pub fn new() -> Self {
-//         FeedController {
-//             buf: Buffer::default(),
-//         }
-//     }
-//
-//     pub async fn load_subscription(&mut self, sub: Subscription) -> Result<()> {
-//         let content = reqwest::get(&sub.url).await?.bytes().await?;
-//         let channel = Channel::read_from(&content[..])?;
-//
-//         // self.write_outputs(format!("{:?}", channel).as_str())?;
-//         self.buf.rows = channel
-//             .items()
-//             .iter()
-//             .map(|item| item.title().unwrap_or(&"unknown title").to_string())
-//             .collect::<Vec<_>>();
-//         Ok(())
-//     }
-//
-//     fn write_outputs(&self, out: &str) -> Result<()> {
-//         let path = Path::new("channel_output.txt");
-//
-//         // Open a file in write-only mode, returns `io::Result<File>`
-//         let mut file = File::create(&path)?;
-//
-//         // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
-//         file.write_all(out.as_bytes())?;
-//         Ok(())
-//     }
-// }
+use anyhow::anyhow;
 
 pub struct Feeding {
     pub buf: Buffer,
--- src/subscription.rs
+++ src/subscription.rs
@@ -1,12 +1,4 @@
-use std::io::{BufRead, BufReader};
-use std::{env::current_dir, vec};
-
 use crate::Buffer;
-use crate::Result;
-pub struct SubscriptionsController {
-    pub buf: Buffer,
-    pub subscriptions: Vec<Subscription>,
-}
 
 #[derive(Debug, Clone)]
 pub struct Subscription {
@@ -21,82 +13,4 @@ impl From<Vec<Subscription>> for Buffer {
             rows: items.iter().map(|x| x.url.clone()).collect::<Vec<_>>(),
         }
     }
-}
-
-impl SubscriptionsController {
-    pub fn debug() -> Self {
-        Self {
-            buf: Buffer {
-                cx: 1,
-                cy: 1,
-                rows: "hello world what the hell are you doing"
-                    .split(' ')
-                    .map(|x| x.to_string())
-                    .collect::<Vec<_>>(),
-            },
-            subscriptions: vec![],
-        }
-    }
-
-    pub fn new() -> Result<Self> {
-        let curr_dif = current_dir()
-            .expect("failed to get current dir")
-            .to_str()
-            .unwrap()
-            .to_string();
-
-        SubscriptionsController::from_url_file(Some(curr_dif + "/urls"))
-    }
-
-    pub fn select(&self) -> Subscription {
-        self.subscriptions
-            .get(self.buf.cy - 0)
-            .expect("failed to get the selected subsctription")
-            .clone()
-    }
-
-    pub fn buf_mut(&mut self) -> &mut Buffer {
-        &mut self.buf
-    }
-
-    pub fn buf(&self) -> &Buffer {
-        &self.buf
-    }
-
-    pub fn from_url_file(filepath: Option<String>) -> Result<Self> {
-        // https://stackoverflow.com/a/35820003/7358099
-        use std::fs;
-        if let Some(path) = filepath {
-            let file = fs::File::open(path)?;
-            let buf = BufReader::new(file);
-            let subscriptions = buf
-                .lines()
-                .filter_map(|l| {
-                    if let Ok(url) = l {
-                        if valid_url(&url) {
-                            return Some(Subscription { url });
-                        }
-                    }
-                    None
-                })
-                .collect::<Vec<Subscription>>();
-            return Ok(Self {
-                buf: subscriptions.clone().into(),
-                subscriptions,
-            });
-        }
-        Ok(Self {
-            buf: Buffer {
-                cx: 1,
-                cy: 1,
-                rows: vec![],
-            },
-            subscriptions: vec![],
-        })
-    }
-}
-
-fn valid_url(url: &str) -> bool {
-    // TODO
-    url.len() > 0
 }
